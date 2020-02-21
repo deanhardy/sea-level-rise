67dc5d107744c3b9d439bff855a19bb13d0c2754
Dean Hardy <dhardy@sesync.org>
2020-02-20 21:43:59 -0500

    trying to improve first date cutoff

--- scripts/sea-level.R
+++ scripts/sea-level.R
@@ -17,7 +17,7 @@ datadir <- '/Users/dhardy/Dropbox/r_data/sea-level-rise'
 #                  stringsAsFactors = FALSE)
 
 ## define variables
-STATION <- c(8670870, 8720030) ## define stations
+STATION <- c(8661070) ## define stations 8720030, 8670870, 
 DATUM <- 'MSL' ## define datum
 P <- seq(0,8,1) ## define number of decades of data to grab where 0 = 1 decade, 1 = 2 decades, etc
 df <- NULL ## empty dataframe
@@ -93,11 +93,24 @@ T_name <- ifelse(T == 3653, "Decade",
                                                            ifelse(T == 3653*8, 'Eight Decades', 'Nine Decades'))))))))
 
 ## filter all data to one station and selected time period
-dat2 <- filter(df, station == z & date >= Sys.Date()-T) %>%
-  mutate(MSL = MSL*100) %>%
-  arrange(date)
-m <- lm(MSL ~ date, dat2) ## create regression line
+# dat2 <- filter(df, station == STATION[1]) %>%
+#   mutate(MSL = MSL*100) %>%
+#   arrange(date) %>%
+#   ifelse(first(date) > Sys.Date()-T, filter(dat2$date >= first(date)), filter(dat2$date >= Sys.Date()-T))
 
+dat2 <- filter(df, station == STATION[1]) %>%
+  mutate(MSL = MSL*100) %>%
+  arrange(date) %>%
+filter(
+  if (first(date) > Sys.Date()-T) {
+    date >= first(date)
+  } else {
+    date >= Sys.Date()-T
+  }
+)
+  
+m <- lm(MSL ~ date, dat2) ## create regression line
+  
 ## 
 ## mimic style in NOAA graph here: https://tidesandcurrents.noaa.gov/sltrends/sltrends_station.shtml?id=8670870
 fig2 <- ggplot(dat2, aes(x = date, y = MSL)) +
@@ -125,19 +138,18 @@ fig2 <- ggplot(dat2, aes(x = date, y = MSL)) +
         panel.grid.minor.y = element_line(colour = 'black', linetype = 2),
         axis.ticks.x = element_line(color = 'black')) + 
   ggtitle(paste("Sea Level Trend Over Past ", T_name, sep = ''))  +
-  annotate(geom = 'text', label = paste("Total Observed SLR for Period =", 
-                                        round(coef(m)[2]*T, 2), 'cm'), 
-           x = Sys.Date()-(T), y = Inf, hjust = -0.1, vjust = 5) + 
-  annotate(geom = 'text', label = paste("Linear Relative Sea Level Trend (black line) =", 
-                                        round((coef(m)[2]*T*10/(T/365.3)), 2), 'mm/yr'), 
-           x = Sys.Date()-(T), y = Inf, hjust =-0.08, vjust = 7) + 
+  annotate(geom = 'text', label = paste("Total Observed SLR for Period =",
+                                        round(coef(m)[2]*T, 2), 'cm'),
+           x = Sys.Date()-(T), y = Inf, hjust = -0.1, vjust = 5) +
+  annotate(geom = 'text', label = paste("Linear Relative Sea Level Trend (black line) =",
+                                        round((coef(m)[2]*T*10/(T/365.3)), 2), 'mm/yr'),
+           x = Sys.Date()-(T), y = Inf, hjust =-0.08, vjust = 7) +
   labs(caption = paste("Data: Monthly ", DATUM, " for NOAA Station ID: ", dat2$station, 
                        ', ', first(dat2$date), ' to ', last(dat2$date), sep = ''))
 fig2
 
 # save plots as .png
 ggsave(fig2, file=paste(datadir,
-                       '/figures/sea-level-trends_', i, "-decade_station-", dat2$station[1], ".png", sep=''), width = 6, height = 4, units = 'in', scale=2)
+                       '/figures/', dat2$station[1], '-sea-level-trends_', i, "-decades", ".png", sep=''), width = 6, height = 4, units = 'in', scale=2)
 }
 }
-
