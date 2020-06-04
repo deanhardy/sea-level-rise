631db38db3540147e61c02eeb31d9700eef4da6b
Dean Hardy <dhardy@sesync.org>
2020-06-04 15:07:15 -0400

    playing around with table 2 fig

--- scripts/slr-typology-table2.R
+++ scripts/slr-typology-table2.R
@@ -18,10 +18,18 @@ df <- read.csv(file.path(datadir, 'data/slr-typology-table2.csv'), stringsAsFact
   mutate(Approach = factor(Approach, levels = applevels))
 
 fig <- ggplot(filter(df, Group2 %in% c(1:6, 8:9))) +
+  geom_col(aes(x = reorder(County, Group2), y=0.25, group = (GroupApproach), fill = Group)) + 
   geom_point(aes(x=reorder(County, Group2), y=percent, color = Approach)) +
   geom_path(aes(x=reorder(County, Group2), y=percent, group = (GroupApproach), color = Approach)) +
-  labs(y = "Percent", x = "County") + 
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+  scale_y_continuous(breaks = seq(0,1,0.1), expand = c(0,0)) + 
+  # scale_x_discrete(expand=c(0,0)) +
+  labs(y = "Percent", x = "County") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1),
+        panel.background = element_rect(fill = 'white'),
+        panel.border = element_rect(color = 'black', fill = NA),
+        panel.grid.major.y = element_line(color = 'grey', linetype = 'dashed'),
+        # panel.grid.major.x = element_line(color = 'grey', linetype = 'dashed'),
+        panel.grid.major.x = element_blank())
 fig
 
 tiff(file.path(datadir, 'figures/table2_plotted.tif'), height = 4, width = 6, units = 'in', compression = 'lzw', res = 300)
