18c8bb77b4bf84993e738203d08d35248de8ecc6
Dean Hardy <dhardy@sesync.org>
2020-06-05 10:37:46 -0400

    minor changes

--- scripts/slr-typology-table2.R
+++ scripts/slr-typology-table2.R
@@ -17,11 +17,12 @@ df <- read.csv(file.path(datadir, 'data/slr-typology-table2.csv'), stringsAsFact
                                               ifelse(Approach == 'EAE', str_c(Group, '.4'), Group))))) %>%
   mutate(Approach = factor(Approach, levels = applevels))
 
-fig <- ggplot(filter(df, Group2 %in% c(1:6, 8:9))) +
-  geom_col(aes(x = reorder(County, Group2), y=0.25, group = (GroupApproach), fill = Group)) + 
+fig <- ggplot(filter(df, Group2 %in% c(1:6, 8:9)), x=reorder(County, Group2), y=percent) +
+  # geom_bar(aes(x = reorder(County, Group2), group = Approach)) + 
   geom_point(aes(x=reorder(County, Group2), y=percent, color = Approach)) +
   geom_path(aes(x=reorder(County, Group2), y=percent, group = (GroupApproach), color = Approach)) +
-  scale_y_continuous(breaks = seq(0,1,0.1), expand = c(0,0)) + 
+  scale_y_continuous(breaks = seq(0,1,0.1), expand = c(0,0)) +
+  scale_x_discrete() + 
   # scale_x_discrete(expand=c(0,0)) +
   labs(y = "Percent", x = "County") +
   theme(axis.text.x = element_text(angle = 45, hjust = 1),
@@ -32,6 +33,11 @@ fig <- ggplot(filter(df, Group2 %in% c(1:6, 8:9))) +
         panel.grid.major.x = element_blank())
 fig
 
-tiff(file.path(datadir, 'figures/table2_plotted.tif'), height = 4, width = 6, units = 'in', compression = 'lzw', res = 300)
+tiff(file.path(datadir, 'figures/table2_funplot.tif'), height = 4, width = 6, units = 'in', compression = 'lzw', res = 300)
 fig
 dev.off()
+
+df2 <- data.frame(x = c("a","a","b","b"), y = 2:5, g = rep(1:2, 2))
+p <- ggplot(df2, aes(x, y, group = g)) +
+  geom_col(position = "dodge", fill = "grey50", colour = "black")
+p
